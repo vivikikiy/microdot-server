c25fe05d1df33ef67eb0c85794b8d2cf5d39d1ba
vivikikiy <101606936+vivikikiy@users.noreply.github.com>
2025-04-23 16:17:27 -0300

    Add files via upload

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,31 @@
-# Aplicacion del servidor
+from microdot import Microdot, Response, send_file
+
+app = Microdot()
+
+@app.route('/')
+def index(request):
+    try:
+        return send_file('index.html', content_type='text/html')
+    except Exception as e:
+        print(f"Error al cargar index.html: {e}")
+        return "Error al cargar la página", 500
+
+@app.route('/base.css')
+def css(request):
+    try:
+        return send_file('base.css', content_type='text/css')
+    except Exception as e:
+        print(f"Error al cargar base.css: {e}")
+        return "Error al cargar el CSS", 500
+    
+@app.route('/actividad01.js')
+def css(request):
+    try:
+        return send_file('actividad01.js', content_type='text/css')
+    except Exception as e:
+        print(f"Error al cargar el scripts: {e}")
+        return "Error al cargar el scripts", 500
+
+
+print("Iniciando servidor en 0.0.0.0:5000...")
+app.run(debug=True)
--- actividades/ej_01/boot.py
+++ actividades/ej_01/boot.py
@@ -1,1 +1,48 @@
-# Configuracion inicial
+from machine import Pin, I2C
+import ssd1306
+from time import sleep
+
+def connect_to(ssid: str, passwd: str) -> str:
+    """Conecta el microcontrolador a la red indicada y retorna la dirección IP asignada.
+    
+    Parameters
+    ----------
+    ssid : str
+        Nombre de la red a conectarse
+    passwd : str
+        Contraseña de la red
+        
+    Returns
+    -------
+    str
+        Dirección IP asignada al microcontrolador
+    """
+    
+    import network
+    from time import sleep
+    
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        sta_if.active(True)
+        sta_if.connect(ssid, passwd)
+        while not sta_if.isconnected():
+            sleep(.05)
+    
+
+    return sta_if.ifconfig()[0]
+
+print(connect_to("Cooperadora Alumnos", ""))
+i2c = I2C(0, scl=Pin(22), sda=Pin(21))
+oled = ssd1306.SSD1306_I2C(128, 64, i2c)
+ip = connect_to("Cooperadora Alumnos", "")
+
+
+oled.fill(0)  
+oled.text("#tetas", 0, 0)
+oled.show()
+sleep(2)  
+
+oled.fill(0)  
+oled.text("IP:", 0, 0)
+oled.text(ip, 0, 15)  
+oled.show()
